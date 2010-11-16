1efa98bf4ed5006d4885a99167c70fb48b770c3b
Will Prater <will@thesuperformula.com>
2010-11-15 22:47:15 -0800

    Remove Escape HTML button

--- public/javascripts/thecannalyst/rich_toolbar.js
+++ public/javascripts/thecannalyst/rich_toolbar.js
@@ -3,7 +3,7 @@
     // Remove unwanted buttons from the RichTextArea Toolbar
     $($S).bind('ready', function () {
         if ($(Sugar.ta).length > 0) {
-            $('ul.' + Sugar.ta.toolbar.settings.className + ' .EscapeHTMLButton').parent('li').remove();
+            Sugar.ta.toolbar.removeButton('Escape HTML');
         }
     });
     
